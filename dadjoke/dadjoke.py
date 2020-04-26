0ac9a555705f3990c5cf3490dbfb84f83c8fb8bd
Nikhil Sharma <jangidnik@gmail.com>
2020-04-26 22:30:57 +0530

    Update dadjoke.py

--- dadjoke/dadjoke.py
+++ dadjoke/dadjoke.py
@@ -1,16 +1,27 @@
 import discord
 from discord.ext import commands
+
+import asyncio
+import os
+import random
 import aiohttp
+import humanize
 from datetime import datetime
-from redbot.core import commands, checks, Config
-@commands.command()
-async def dadjoke(self, ctx):
+from utils.settings import GREEN_EMBED, ERROR_EMOJI
+import utils.checks
+from discord.ext.commands.cooldowns import BucketType
+ @commands.command()
+ @commands.cooldown(1,2,BucketType.user)
+ @commands.guild_only()
+ @commands.check(utils.checks.is_bot)
+ async def dadjoke(self, ctx):
+        """Says a dad joke. 2 second cooldown."""
         try:
             headers = {"Accept": "application/json"}
             async with aiohttp.ClientSession() as session:
-                async with session.get('https://icanhazdadjoke.com/', headers=headers) as get:
+                async with session.get('https://icanhazdadjoke.com', headers=headers) as get:
                     resp = await get.json()
-                    embed = discord.Embed(color=ctx.message.author.top_role.colour)
+                    embed = discord.Embed(color=GREEN_EMBED)
                     embed.title = "A dad joke."
                     embed.description = f"{resp['joke']}"
                     embed.set_footer(text=f"{self.bot.user.name}")
