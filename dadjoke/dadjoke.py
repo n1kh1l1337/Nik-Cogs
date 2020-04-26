1adad89246eea72cea065e90fff7cf7129032232
Nikhil Sharma <jangidnik@gmail.com>
2020-04-26 22:48:04 +0530

    Update dadjoke.py

--- dadjoke/dadjoke.py
+++ dadjoke/dadjoke.py
@@ -1,5 +1,4 @@
 import discord
-from discord.ext import commands
 from redbot.core import commands, checks, Config
 import asyncio
 import aiohttp
@@ -9,21 +8,24 @@ from discord.ext.commands.cooldowns import BucketType
 class DadJoke(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
     @commands.command()
-    @commands.cooldown(1,5,BucketType.user)
+    @commands.cooldown(1,2,BucketType.user)
     @commands.guild_only()
-    async def random(self, ctx):
-        """Chooses a random user. 5 second cooldown."""
-        
-        user = random.choice(ctx.guild.members)
-        test = self.bot.get_user(user.id)
-        embed = discord.Embed(color=GREEN_EMBED)
-        embed.description = f"User ID: {user.id}\nBot: {user.bot}\nJoined At: {humanize.naturaldate(user.joined_at)}\nStatus: {user.status}\n```{user.activity}```"
-        embed.set_footer(text=test)
-        embed.set_thumbnail(url=user.avatar_url)
-        embed.timestamp = datetime.utcnow()
-        await ctx.send(embed=embed)
+    async def dadjoke(self, ctx):
+        """Says a dad joke. 2 second cooldown."""
+        try:
+            headers = {"Accept": "application/json"}
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://icanhazdadjoke.com', headers=headers) as get:
+                    resp = await get.json()
+                    embed = discord.Embed(color=black)
+                    embed.title = "A dad joke."
+                    embed.description = f"{resp['joke']}"
+                    embed.set_footer(text=f"{self.bot.user.name}")
+                    embed.timestamp = datetime.utcnow()
+                    await ctx.send(embed=embed)
+        except Exception as e:
+            await ctx.send(f"{e}")
 def setup(bot):
    n = DadJoke(bot)
    bot.add_cog(n)
