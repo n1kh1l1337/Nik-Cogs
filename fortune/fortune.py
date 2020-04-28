04b7f3220b25776b5290d9285eecf8056d3bd389
Nikhil Sharma <jangidnik@gmail.com>
2020-04-28 10:55:39 +0530

    Update fortune.py

--- fortune/fortune.py
+++ fortune/fortune.py
@@ -1,10 +1,30 @@
-import discord
-from redbot.core import commands, checks, Config
+import json
+import random
+from typing import List, Optional
+
+from redbot.core import commands
+from redbot.core.utils.chat_formatting import box
+from redbot.core.data_manager import bundled_data_path
 from datetime import datetime
+
+from .cows import cowsay
+
+class Moo:
+    @classmethod
+    async def convert(cls, ctx, arg):
+        if arg[:3].lower() == "moo":
+            return cls()
+
+        raise commands.BadArgument()
+
 
 class Fortune(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._data: List[str]
+        path = bundled_data_path(self) / "fortunes.json"
+        with path.open(mode="r", encoding="utf-8") as fp:
+            self._data = json.load(fp)
     @commands.cooldown(1, 30)
     @commands.command(usage="")
     async def fortune(self, ctx: commands.Context, what_does_the_cowsay: Optional[Moo] = None):
@@ -22,6 +42,22 @@ class Fortune(commands.Cog):
         embed.set_footer(text=f"{self.bot.user.name}")
         embed.timestamp = datetime.utcnow()
         await ctx.send(embed=embed)
+        
+    @commands.cooldown(1, 30)
+    @commands.command(hidden=True, name="cowsay")
+    async def _cowsay(self, ctx: commands.Context, *, what_does_the_cowsay: str = ""):
+        """
+        Moo.
+        """
+        if not what_does_the_cowsay:
+            await ctx.send_help()
+            return
+
+        if len(what_does_the_cowsay) > 1500:
+            await ctx.send("Moo.")
+            return
+
+        await ctx.send(box(cowsay(what_does_the_cowsay)))
 def setup(bot):
    n = Fortune(bot)
    bot.add_cog(n)
