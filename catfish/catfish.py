3bee745a26260d2af6a2212300ff061cf1f4c759
Nikhil Sharma <jangidnik@gmail.com>
2020-04-28 11:26:31 +0530

    Update catfish.py

--- catfish/catfish.py
+++ catfish/catfish.py
@@ -76,14 +76,14 @@ class Catfish(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def catfish(self, ctx, member : discord.Member):
+    async def catfish(self, ctx: commands.Context, user : discord.Member):
         # avatarUrl = user.avatar_url
         # for similarImg in parseResults(doImageSearch(user.avatar_url))[2]:
         #     print(similarImg)
 
-        msgReply = 'Top 3 similar images to ' + member.name + '#' + member.discriminator + '\'s avatar:\n'
+        msgReply = 'Top 3 similar images to ' + user.name + '#' + user.discriminator + '\'s avatar:\n'
 
-        res = json.loads(parseResults(doImageSearch(member.avatar_url)))
+        res = json.loads(parseResults(doImageSearch(user.avatar_url)))
         # for img in res['similar_images']:
         #     # print('img: ' + img)
         #     msgReply += img + '\n'
