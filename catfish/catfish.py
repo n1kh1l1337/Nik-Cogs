8f211ff1a38376f44ed0f6dda060c7b12ce8c4c1
Nikhil Sharma <jangidnik@gmail.com>
2020-05-01 17:49:44 +0530

    oop

--- catfish/catfish.py
+++ catfish/catfish.py
@@ -53,7 +53,7 @@ def parseResults(code):
 
     for div in soup.findAll('div', attrs={'class':'g'}):
         sLink = div.find('a')
-        await ctx.send(sLink)
+        print(sLink)
         results['links'].append(sLink['href'])
 
     for desc in soup.findAll('span', attrs={'class':'st'}):
