7ab733037821b91992d687bd515df27a0ea6bc71
Nikhil Sharma <jangidnik@gmail.com>
2020-05-05 00:45:08 +0530

    yomama test

---
+++ yomamma/yomama.py
@@ -0,0 +1,31 @@
+import discord
+from redbot.core import commands, checks, Config
+import asyncio
+import aiohttp
+from datetime import datetime
+from discord.ext.commands.cooldowns import BucketType
+
+class YoMama(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command()
+    @commands.cooldown(1,2,BucketType.user)
+    @commands.guild_only()
+    async def yomama(self, ctx):
+        """Says a Yomama joke. 2 second cooldown."""
+        try:
+            headers = {"Accept": "application/json"}
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://api.yomomma.info/', headers=headers) as get:
+                    resp = await get.json()
+                    embed = discord.Embed(color=ctx.message.author.top_role.colour)
+                    embed.title = "yomama!."
+                    embed.description = f"{resp['joke']}"
+                    embed.set_footer(text=f"{self.bot.user.name}")
+                    embed.timestamp = datetime.utcnow()
+                    await ctx.send(embed=embed)
+        except Exception as e:
+            await ctx.send(f"{e}")
+def setup(bot):
+   n = YoMama(bot)
+   bot.add_cog(n)
