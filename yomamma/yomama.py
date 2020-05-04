2521c26717657606ab88d74d787afed537e3b38b
Nikhil Sharma <jangidnik@gmail.com>
2020-05-05 01:03:45 +0530

    once again

--- yomamma/yomama.py
+++ yomamma/yomama.py
@@ -2,6 +2,7 @@ import discord
 from redbot.core import commands, checks, Config
 import asyncio
 import aiohttp
+import json
 from datetime import datetime
 from discord.ext.commands.cooldowns import BucketType
 
@@ -16,10 +17,10 @@ class YoMama(commands.Cog):
         try:
             async with aiohttp.ClientSession() as session:
                  async with session.get("https://api.yomomma.info/") as response:
-                    result = await response.json()
+                    result = await response.json(content_type='text/html')
                     embed = discord.Embed(color=ctx.message.author.top_role.colour)
                     embed.title = "yomama!."
-                    embed.description = f"{result[0]['joke']}"
+                    embed.description = f"{result['joke']}"
                     embed.set_footer(text=f"{self.bot.user.name}")
                     embed.timestamp = datetime.utcnow()
                     await ctx.send(embed=embed)
