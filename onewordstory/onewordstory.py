2258b00a2b9a182be83705ff6b24bbabec6ec28b
Nikhil Sharma <jangidnik@gmail.com>
2020-05-05 23:04:17 +0530

    test

--- onewordstory/onewordstory.py
+++ onewordstory/onewordstory.py
@@ -450,7 +450,7 @@ class OneWordStory(commands.Cog):
                 # If the amount of words is over 1, add an s to "word(s)".
                 if max_words_allowed > 1:
                     maybe_s_string = "s"
-               # wordmsg = await ctx.send(f"**Story so far**\n```{start_line}...```\nAlright {tempuser.mention}, give me at most **{max_words_allowed}** word{maybe_s_string}! *{current} seconds remaining...*")
+                wordmsg = await ctx.send(f"**Story so far**\n```{start_line}...```\nAlright {tempuser.mention}, give me at most **{max_words_allowed}** word{maybe_s_string}! *{current} seconds remaining...*")
                                          #.format(start_line=start_line, max_words_allowed=max_words_allowed, user_mention=tempuser.mention, current=current, maybe_s_string=maybe_s_string))
                 
                 
