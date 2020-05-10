f5ce69fb53a6220bdc4b7ebf1ef24aa83058f63f
Nikhil Sharma <jangidnik@gmail.com>
2020-05-10 17:23:02 +0530

    squash! roast

--- roast/roast.py
+++ roast/roast.py
@@ -22,13 +22,9 @@ class Roast(commands.Cog):
                 async with session.get("https://insult.mattbas.org/api/insult.json",
                                               headers=headers) as get:
                     res = await get.json(content_type='text/json')
-                    if res.status != 200:
-                        await ctx.send("That lucky bastard... An error occurred."
-                               "Mission failed bois, we'll get 'em next time")
-                        return
                     embed = discord.Embed(color=ctx.message.author.top_role.colour)
                     embed.title = "Roast"
-                    embed.description = f"{member.mention},{res["insult"]}"
+                    embed.description = f"{member.mention},{res['insult']}"
                     embed.set_footer(text=f"{self.bot.user.name}")
                     embed.timestamp = datetime.utcnow()
                     await ctx.send(embed=embed)
