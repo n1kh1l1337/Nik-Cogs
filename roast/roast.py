f5c7319649fb3361d4e338526ebb5fa5d4d4f758
Nikhil Sharma <jangidnik@gmail.com>
2020-05-10 16:53:06 +0530

    squash! roast

--- roast/roast.py
+++ roast/roast.py
@@ -1,6 +1,6 @@
-import discord
 from datetime import datetime
-from redbot.core import command
+import discord
+from redbot.core import commands
 class Roast(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
