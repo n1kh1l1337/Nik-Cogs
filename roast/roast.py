c61e707c5e06e271448c62036bdeaad57497e0e7
Nikhil Sharma <jangidnik@gmail.com>
2020-05-10 17:10:51 +0530

    squash! roast

--- roast/roast.py
+++ roast/roast.py
@@ -24,13 +24,12 @@ class Roast(commands.Cog):
                     await ctx.send("That lucky bastard... An error occurred."
                                "Mission failed bois, we'll get 'em next time")
                     return
-                    insult = (await res.json(content_type="text/json"))['insult']
-                    embed = discord.Embed(color=ctx.message.author.top_role.colour)
-                    embed.title = "Roast"
-                    embed.description = f"{member.mention}, {insult}"
-                    embed.set_footer(text=f"{self.bot.user.name}")
-                    embed.timestamp = datetime.utcnow()
-                    await ctx.send(embed=embed)
+                embed = discord.Embed(color=ctx.message.author.top_role.colour)
+                embed.title = "Roast"
+                embed.description = f"{member.mention}, {res['insult']}"
+                embed.set_footer(text=f"{self.bot.user.name}")
+                embed.timestamp = datetime.utcnow()
+                await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Roast(bot))
