e80281d6a9a8fb926a9d13c4d271216b186cce97
Nikhil Sharma <jangidnik@gmail.com>
2020-05-12 02:27:02 +0530

    cause fuck it

--- util/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bloody_menu_pages import BloodyMenuPages, EmbedPagesData, TextPagesData
-from .timers import Timers
-from .discord_logging_handler import DiscordLoggingHandler
-from .funs import *
-from .consts import *
--- util/consts.py
+++
@@ -1,5 +0,0 @@
-API_BASE_URL = 'https://discordapp.com/api'
-AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
-TOKEN_URL = API_BASE_URL + '/oauth2/token'
-
-SUPPORT_SERVER_INVITE = None
