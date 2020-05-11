68fd6196066cf8aa32f8b7c00a97f219c338e8af
Nikhil Sharma <jangidnik@gmail.com>
2020-05-12 02:20:15 +0530

    idk

--- reminder/reminder.py
+++ reminder/reminder.py
@@ -1,10 +1,59 @@
 import discord
 from redbot.core import commands
 import textwrap
-import util
 from resources import Timer
 import pendulum
-from util.converters import HumanTime
+
+class HumanTime(commands.Converter):
+    def __init__(self, converter: commands.Converter = None, other=False):
+        self.other = other
+        self.other_converter = converter
+
+    class HumanTimeOutput:
+        def __init__(self, time, other=None):
+            self.time = time
+            self.other = other
+
+    def parse(self, user_input, ctx):
+        settings = {
+            'TIMEZONE': 'UTC',
+            'RETURN_AS_TIMEZONE_AWARE': True,
+            'TO_TIMEZONE': 'UTC',
+            'PREFER_DATES_FROM': 'future'
+        }
+
+        to_be_passed = f"in {user_input}"
+        split = to_be_passed.split(" ")
+        length = len(split[:7])
+        out = None
+        used = ""
+        for i in range(length, 0, -1):
+            used = " ".join(split[:i])
+            out = dateparser.parse(used, settings=settings)
+            if out is not None:
+                break
+
+        if out is None:
+            raise commands.BadArgument('Provided time is invalid')
+
+        now = ctx.message.created_at
+        return out.replace(tzinfo=now.tzinfo), ''.join(to_be_passed).replace(used, '')
+
+    def time_check(self, time, ctx):
+        now = ctx.message.created_at
+        if time is None:
+            raise commands.BadArgument('Provided time is invalid')
+        elif time < now:
+            raise commands.BadArgument('Time is in past')
+
+    async def convert(self, ctx, argument):
+        time, other = await ctx.bot.loop.run_in_executor(None, functools.partial(self.parse, argument, ctx))
+        self.time_check(time, ctx)
+        if self.other_converter is not None:
+            other = await self.other_converter.convert(ctx, argument)
+        return HumanTime.HumanTimeOutput(time, other)
+
+
 class Reminders(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -44,7 +93,7 @@ class Reminders(commands.Cog):
 
         embed = discord.Embed(
             title='Upcoming reminders',
-            color=util.random_discord_color()
+            color=ctx.message.author.top_role.colour
         )
         embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
 
