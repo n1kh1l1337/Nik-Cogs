ef79f77952f8bca9dfe8ad0836ba8da50f0dda24
Nikhil Sharma <jangidnik@gmail.com>
2020-05-22 23:40:01 +0530

    down

--- guess/guess.py
+++ guess/guess.py
@@ -7,15 +7,15 @@ class Guess(commands.Cog):
         self.bot = bot
     @commands.command()
     async def guess(self,ctx):
-        """Guess a number between 1 to 100"""
-        embed = discord.Embed(title='Guess a Number', description='Between 1 and 100',colour=0x00ff00)
+        """Guess a number between 1 to 50"""
+        embed = discord.Embed(title='Guess a Number', description='Between 1 and 50',colour=0x00ff00)
         await ctx.send(embed=embed)
         count =0
         def check(m):
             return m.content.isdigit()
         guess = await self.bot.wait_for('message', check=check)
         winner=guess.author.mention
-        answer = random.randint(1,100)
+        answer = random.randint(1,50)
         while int(guess.content) != answer:
             guess = await self.bot.wait_for('message', check=check)
             count+=1
